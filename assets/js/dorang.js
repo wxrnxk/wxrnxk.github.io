5054f3d6813f078135f3de3cbe483fe84e965738
carliwar <basscarland@hotmail.com>
2025-02-13 12:50:40 -0500

    switch to corvus corax release

--- assets/js/dorang.js
+++ assets/js/dorang.js
@@ -12,56 +12,81 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
- // toggle 
-$(document).ready(function(){
-    
-    $('.search-toggle').click(function(){
-        $('.search-wrapper').toggleClass('show');
-    });
+// toggle
+$(document).ready(function () {
+  var widget = SC.Widget(document.getElementById("corvusIframe"));
+  $(".search-toggle").click(function () {
+    $(".search-wrapper").toggleClass("show");
+  });
 
-    $('.modal-toggle').click(function(){
-        $('.modalBox').toggleClass('show');
-    })
+  $(".modal-toggle").click(function () {
+    $(".modalBox").toggleClass("show");
+    $("#corvusForm").removeClass('hidden'); 
+    $("#alertMessage").hide();   
+    $("#corvusPassword").val('');   
+    $("#corvusCorax").hide();
+    $("#corvusForm").show();
+  });
 
-    $('.modalBox').click(function(){
-        $(this).removeClass('show');
-    });
+  $(document).on("click", function (event) {
+    if (
+      !$(event.target).closest(".modal-content").length &&
+      !$(event.target).closest(".modal-toggle").length &&
+      $("#modalCorvus").is(":visible")
+    ) {
+      $(".modalBox").removeClass("show");
+      $("#corvusCorax").removeClass("show");
+      // TODO stop music if closed
+      widget.pause();
+    }
+  });
 
-    $('.spinner').click(function(){
-        $(".theme-selector").toggleClass('show');
-    });
-    $('.light').click(function(){
-        $('body').addClass('light-theme');
-        $('body').removeClass('dark-theme');
-        $('.dante-item').removeClass('hidden');
-        $('.light-item').addClass('hidden');
-    });
-    $('.dark').click(function(){
-        $('body').toggleClass('dark-theme');
-        $('body').removeClass('light-theme');
-        $('.dante-item').addClass('hidden');
-        $('.light-item').removeClass('hidden');
-    });
+  $("#corvusForm").on("submit", function (event) {
+    event.preventDefault(); // Prevent page reload
+    const password = $("#corvusPassword").val();
+    if (password === "INMATERIALES777") {
+      $("#corvusForm").hide();
+      $("#corvusCorax").show();
+    } else {
+      $("#alertMessage").show();
+      $("#corvusCorax").hide();
+    }
+  });
 
-    
+  $(".spinner").click(function () {
+    $(".theme-selector").toggleClass("show");
+  });
+  $(".light").click(function () {
+    $("body").addClass("light-theme");
+    $("body").removeClass("dark-theme");
+    $(".dante-item").removeClass("hidden");
+    $(".light-item").addClass("hidden");
+  });
+  $(".dark").click(function () {
+    $("body").toggleClass("dark-theme");
+    $("body").removeClass("light-theme");
+    $(".dante-item").addClass("hidden");
+    $(".light-item").removeClass("hidden");
+  });
 });
 
-
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-        if (this.hash !== "") {
+      var hash = this.hash;
 
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
-}); 
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
